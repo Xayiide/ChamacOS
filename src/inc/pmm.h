c24c5aec3d8e84d6024dbdfaa79befde36f9dc6d
Xayiide <wickedcheerio@protonmail.com>
2023-04-03 17:40:33 +0200

    part: poco progreso con el gestor de memoria física.

--- src/inc/pmm.h
+++ src/inc/pmm.h
@@ -4,13 +4,31 @@
 #include <stdint.h>
 #include "multiboot.h"
 
+#define PMM_BASE_ADDR  0x00200000
 #define PMM_FRAME_SIZE 0x1000 /* 4096 */
 #define PMM_NUM_FRAMES 100
+#define PMM_MEM_SIZE   PMM_FRAME_SIZE * PMM_NUM_FRAMES /* 400 KB */
+
+/* Frame entry */
+#define PMM_FRAME_FREE  0x00
+#define PMM_FRAME_USED  0x01
+#define PMM_FRAME_FIRST 0x80
+#define PMM_FRAME_NEXT  0x40
+
+typedef struct 
+{
+    uint32_t installed_mem; /* RAM total instalada (128 MB)  */
+    uint32_t usable_mem;    /* RAM total que usamos (400 KB) */
+    uint32_t free_mem;      /* De la RAM usable, la no usada */
+    uint8_t *map_addr;
+    uint32_t map_size;
+} pmm_info_t;
+
 
 /* Tenemos que almacenar la siguiente información:
  * - Dir inicio: 0x0020_0000 (Empezado el segundo MB para no borrar info
  *               del kernel)
- * - Tamaño mapa: 8 * PMM_NUM_FRAMES (1 byte por frame)
+ * - Tamaño mapa: PMM_NUM_FRAMES (1 byte por frame) (x8 para tam. en bits)
  * - Inicio mapa: Dir inicio + Tamaño mapa
  * 
  * Con esto ya podríamos empezar.
@@ -24,6 +42,9 @@
 
 
 
+
 void pmm_init(multiboot_info_t *mbd, uint32_t magic);
+
+pmm_info_t *pmm_get_meminfo(void);
 
 #endif
--- src/pmm.c
+++ src/pmm.c
@@ -8,14 +8,18 @@
 
 typedef multiboot_memory_map_t multiboot_mmap_t; /* Ahorrar espacio */
 
-static void pmm_validate(multiboot_info_t *mbd, uint32_t magic);
+/* Definiciones estáticas */
+static pmm_info_t pmm_meminfo =
+    {.installed_mem = 0,
+     .usable_mem    = PMM_MEM_SIZE,
+     .free_mem      = 0,
+     .map_addr      = (uint8_t *) PMM_BASE_ADDR,
+     .map_size      = PMM_NUM_FRAMES};
 
-uint32_t  total_memory = 0;
-uint32_t  free_memory  = 0;
-
-uint8_t  *pmm_bitmap   = NULL;
-uint32_t  bitmap_size  = PMM_NUM_FRAMES * sizeof(uint8_t);
-
+static void    pmm_validate(multiboot_info_t *mbd, uint32_t magic);
+static void    pmm_printinfo(multiboot_info_t *mbd);
+static void    pmm_map_frame_set(uint32_t index, uint8_t status);
+static uint8_t pmm_map_frame_get(uint32_t index);
 
 static void pmm_validate(multiboot_info_t *mbd, uint32_t magic)
 {
@@ -31,10 +35,8 @@ static void pmm_validate(multiboot_info_t *mbd, uint32_t magic)
 
 }
 
-void pmm_init(multiboot_info_t *mbd, uint32_t magic)
+static void pmm_printinfo(multiboot_info_t *mbd)
 {
-    pmm_validate(mbd, magic);
-    
     vga_color(VGA_BACK_BLACK, VGA_FORE_GREEN);
     for (int i = 0; i < mbd->mmap_length; i += sizeof(multiboot_mmap_t))
     {
@@ -47,13 +49,12 @@ void pmm_init(multiboot_info_t *mbd, uint32_t magic)
             mmmt->type == MULTIBOOT_MEMORY_NVS       ||
             mmmt->type == MULTIBOOT_MEMORY_BADRAM)
         {
-            total_memory += mmmt->len_lo;
+            pmm_meminfo.installed_mem += mmmt->len_lo;
         }
 
         if (mmmt->type == MULTIBOOT_MEMORY_AVAILABLE &&
             mmmt->addr_lo >= 0x00100000) /* punto de entrada */
         {
-            pmm_bitmap  = (uint8_t *) mmmt->addr_lo;
             vga_color(VGA_BACK_BLACK, VGA_FORE_CYAN);
             printk("Memoria usable: ");
             printk("%d KBs a partir de la dir. 0x%x\n",
@@ -63,17 +64,52 @@ void pmm_init(multiboot_info_t *mbd, uint32_t magic)
         }
     }
 
-    printk("pmm_bitmap: 0x%x\n", pmm_bitmap);
-    printk("pmm_bitmap size: %d B\n", bitmap_size);
+    printk("pmm_bitmap: 0x%x\n", (uint32_t ) pmm_meminfo.map_addr);
+    printk("pmm_bitmap size: %d B (%d bits)\n", pmm_meminfo.map_size,
+        pmm_meminfo.map_size * 8);
 
-    printk("Memoria total: 0x%x (%d MB)\n", total_memory,
-        total_memory/1024/1024);
+    printk("Memoria instalada: 0x%x (%d MB)\n", pmm_meminfo.installed_mem,
+        pmm_meminfo.installed_mem/1024/1024);
 
     printk("Tamano de pagina:  %d Bytes\n", PMM_FRAME_SIZE);
     printk("Numero de paginas: %d\n", PMM_NUM_FRAMES);
-    printk("Total de memoria fisica: %x\n", PMM_FRAME_SIZE * PMM_NUM_FRAMES);
-    printk("                        (%d)\n", PMM_FRAME_SIZE * PMM_NUM_FRAMES);
-    printk("                        (%d KB)\n", (PMM_FRAME_SIZE * PMM_NUM_FRAMES) / 1024);
+    printk("Memoria fisica usable: %d KB\n", pmm_meminfo.usable_mem / 1024);
 
     vga_color(VGA_BACK_BLACK, VGA_FORE_WHITE);
+
+}
+
+static void pmm_map_frame_set(uint32_t index, uint8_t status)
+{
+    pmm_meminfo.map_addr[index] = status;
+}
+
+static uint8_t pmm_map_frame_get(uint32_t index)
+{
+    uint8_t frame;
+
+    frame = pmm_meminfo.map_addr[index];
+
+    return frame;
+}
+
+
+
+
+
+
+void pmm_init(multiboot_info_t *mbd, uint32_t magic)
+{
+    pmm_validate(mbd, magic);
+    pmm_printinfo(mbd);
+
+    memset(pmm_meminfo.map_addr, PMM_FRAME_FREE, pmm_meminfo.map_size);
+
+    
+}
+
+
+pmm_info_t *pmm_get_meminfo(void)
+{
+    return &pmm_meminfo;
 }
