a3e430a02e944d42db1cc4bab4fa640b5a1942ec
Xayiide <wickedcheerio@protonmail.com>
2023-04-11 19:04:12 +0200

    fix: Corregido error en inb (el problema con el teclado era este

--- src/io.c
+++ src/io.c
@@ -6,7 +6,7 @@
 uint8_t inb(uint16_t port)
 {
     uint8_t ret = 0;
-    __asm__ volatile("inb %1, %0" : : "a" (ret), "Nd" (port));
+    asm volatile("inb %1, %0" : "=a" (ret) : "Nd" (port));
 
     return ret;
 }
