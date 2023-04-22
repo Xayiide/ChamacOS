473c36508c0f8962997615ff9304e0f8931a3daa
Xayiide <wickedcheerio@protonmail.com>
2023-04-22 22:51:22 +0200

    nuevo-makefile: se añade el inicio de la nueva organización del Makefile. No me termina de convencer, no está tan bien organizado como el de linux 0.01 y el makefile va a quedar medio feo

---
+++ src2/boot/boot.asm
@@ -0,0 +1,224 @@
+[BITS 32]
+
+global start
+start:
+    mov esp, _sys_stack
+    jmp stublet
+
+ALIGN 4
+mboot:
+    ; Multiboot macros to make a few lines later more readable
+    MULTIBOOT_PAGE_ALIGN	equ 1<<0
+    MULTIBOOT_MEMORY_INFO	equ 1<<1
+    MULTIBOOT_AOUT_KLUDGE	equ 1<<16
+    MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002
+    MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO | MULTIBOOT_AOUT_KLUDGE
+    MULTIBOOT_CHECKSUM	equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+    EXTERN code, bss, end
+
+    ; This is the GRUB Multiboot header. A boot signature
+    dd MULTIBOOT_HEADER_MAGIC
+    dd MULTIBOOT_HEADER_FLAGS
+    dd MULTIBOOT_CHECKSUM
+
+    dd mboot
+    dd code
+    dd bss
+    dd end
+    dd start
+
+stublet:
+    extern kmain
+    push eax        ; multiboot: magic
+    push ebx        ; multiboot: info structure
+    call kmain
+
+    jmp $
+
+
+; GDT
+global gdt_load
+extern gdtr         ; gdt.c
+gdt_load:
+    lgdt[gdtr]      ; Carga el GDT con la variable gdtr de gdt.c
+    mov ax, 0x10    ; 0x10 es el despl. de nuestro segmento de datos en el GDT
+
+    mov ds, ax      ; Inicializa los registros de segmento
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    
+    jmp 0x08:flush2 ; 0x08 es el despl. del segmento de codigo
+flush2:
+    ret             ; Retorna al codigo de C
+
+
+; ISRs
+global idt_load
+global isr_vector
+
+extern idtr              ; idt.c
+extern isr_fault_handler ; isr.c
+
+idt_load:
+    lidt[idtr]
+    ret
+
+%macro isr_err_stub 1
+isr_stub_%+%1:
+    cli
+    push byte %1  ; Apilo no. de interrupt. El no. de error ya lo apila la CPU
+    jmp isr_common_stub
+%endmacro
+
+%macro isr_no_err_stub 1
+isr_stub_%+%1:
+    cli
+    push byte 0  ; Apilo no. de error de mentira (Vol3: 6.12)
+    push byte %1 ; Apilo no. de interrupt.
+    jmp isr_common_stub
+%endmacro
+
+ ; Volume 3: 6.3.1 (table 6-1)
+isr_no_err_stub 0  ; Divide error
+isr_no_err_stub 1  ; Debug exception
+isr_no_err_stub 2  ; NMI interrupt
+isr_no_err_stub 3  ; Breakpoint
+isr_no_err_stub 4  ; Overflow
+isr_no_err_stub 5  ; BOUND range exceeded
+isr_no_err_stub 6  ; Invalid opcode (undefined opcode)
+isr_no_err_stub 7  ; Device not available (no math coprocessor)
+isr_err_stub    8  ; Double Fault
+isr_no_err_stub 9  ; Coprocessor segment overrun (reserved)
+isr_err_stub    10 ; Invalid TSS
+isr_err_stub    11 ; Segment not present
+isr_err_stub    12 ; Stack-segment fault
+isr_err_stub    13 ; General Protection
+isr_err_stub    14 ; Page Fault
+isr_no_err_stub 15   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 16 ; x87 FPU Floating-point Error (Math fault)
+isr_err_stub    17 ; Alignment check
+isr_no_err_stub 18 ; Machine check
+isr_no_err_stub 19 ; SIMD Floating-point Exception
+isr_no_err_stub 20 ; Virtualization exception
+isr_no_err_stub 21 ; Control Protection exception
+isr_no_err_stub 22   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 23   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 24   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 25   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 26   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 27   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 28   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 29   ; INTEL RESERVED DO NOT USE
+isr_err_stub    30   ; INTEL RESERVED DO NOT USE
+isr_no_err_stub 31   ; INTEL RESERVED DO NOT USE
+                     ; 32-255 User defined
+
+
+isr_common_stub:
+    pusha
+    push ds ; Guarda registros de segmento
+    push es
+    push fs
+    push gs
+
+    mov ax, 0x10 ; Segmento de datos del kernel
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov eax, esp
+    push eax
+    mov eax, isr_fault_handler
+    call eax
+    pop eax
+
+    pop gs
+    pop gs
+    pop es
+    pop ds
+    popa
+
+    add esp, 8
+    iret
+
+isr_vector:
+%assign i 0
+%rep 32
+    dd isr_stub_%+i
+%assign i i+1
+%endrep
+
+
+
+; IRQs (PIC)
+extern irq_fault_handler
+global irq_vector
+
+%macro irq_stub 1
+irq_stub_%+%1:
+    cli
+    push byte 0
+    push byte %1
+    jmp irq_common_stub
+%endmacro
+
+irq_stub 32
+irq_stub 33
+irq_stub 34
+irq_stub 35
+irq_stub 36
+irq_stub 37
+irq_stub 38
+irq_stub 39
+irq_stub 40
+irq_stub 41
+irq_stub 42
+irq_stub 43
+irq_stub 44
+irq_stub 45
+irq_stub 46
+irq_stub 47
+
+
+irq_common_stub:
+    pusha
+    push ds
+    push es
+    push fs
+    push gs
+    
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    mov eax, esp
+    push eax
+    mov eax, irq_fault_handler
+    call eax
+    pop eax
+    
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popa
+    
+    add esp, 8
+    iret
+
+irq_vector:
+%assign i 32
+%rep 16
+    dd irq_stub_%+i
+%assign i i+1
+%endrep
+
+
+SECTION .bss
+    resb 8192 ; Reserva 8192 bytes de memoria
+_sys_stack:
---
+++ src2/idt/isr.c
@@ -0,0 +1,65 @@
+#include "isr.h"
+#include "idt.h"    /* idt_set    */
+#include "sys.h"    /* regs_t     */
+#include "vga.h"    /* vga_puts   */
+#include "gdt.h"    /* gdt macros */
+
+extern void *isr_vector[];
+
+char *isr_exception_name[] =
+{
+    "Divide error",
+    "Debug",
+    "Non Maskable Interrupt (NMI)",
+    "Breakpoint",
+    "Overflow",
+    "BOUND range exceeded",
+    "Invalid opcode",
+    "Device not available (no math coprocessor)",
+    "Double fault",
+    "ISR 9: Reserved (Coprocessor segment overrun)",
+    "Invalid TSS",
+    "Segment not present",
+    "Stack-segment fault",
+    "General protection fault",
+    "Page fault",
+    "ISR 15: Reserved",
+    "x87 FPU error (math)",
+    "Alignment check",
+    "Machine check",
+    "SIMD FP error",
+    "Virtualization error",
+    "Control protection exception",
+    "ISR 22: Reserved",
+    "ISR 23: Reserved",
+    "ISR 24: Reserved",
+    "ISR 25: Reserved",
+    "ISR 26: Reserved",
+    "ISR 27: Reserved",
+    "ISR 28: Reserved",
+    "ISR 29: Reserved",
+    "ISR 30: Reserved",
+    "ISR 31: Reserved"
+};
+
+void isr_init()
+{
+    int i;
+    for (i = 0; i < IDT_NUM_EXC; i++)
+    {
+        idt_set(i, (uint32_t) isr_vector[i],
+                GDT_OFFSET_KERNEL_CODE, IDT_R0_32_INT);
+    }
+}
+
+void isr_fault_handler(regs_t *r)
+{
+    if (r->int_no < IDT_NUM_EXC)
+    {
+        vga_color(VGA_BACK_BLACK, VGA_FORE_RED);
+        vga_puts(isr_exception_name[r->int_no]);
+        vga_puts("\nParando el sistema!\n");
+        vga_color(VGA_BACK_BLACK, VGA_FORE_WHITE);
+        while (1);
+    }
+}
